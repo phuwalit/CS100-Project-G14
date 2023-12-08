6c9cfb3eec1064b5e26a7167a5691b99cfc1e4e1
Jerry <sattawut.vic@dome.tu.ac.th>
2023-12-08 16:07:30 +0700

    v1

--- CSTU Passport/JS/script.js
+++ CSTU Passport/JS/script.js
@@ -1,3 +1,7 @@
+const config = {
+  backendUrl: "http://localhost:8000/", // Default backend URL
+};
+const port = 8000;
 
 function clearField(){
   document.getElementById('fullname').value='';
@@ -63,6 +67,40 @@ function clearField(){
     validateStudentID();
     validateEmail();
   }
+  // Function to fetch activity types from the backend
+async function fetchActivityTypes() {
+  try {
+    const response = await fetch(config.backendUrl + "getActivityType");
+    if (response.ok) {
+      const data = await response.json();
+      return data;
+    } else {
+      console.error("Failed to fetch activity types.");
+      return [];
+    }
+  } catch (error) {
+    console.error("An error occurred while fetching activity types:", error);
+    return [];
+  }
+}
+
+// Function to populate activity types in the select element
+function populateActivityTypes(activityTypes) {
+  const activityTypeSelect = document.getElementById("activityType");
+
+  for (const type of activityTypes) {
+    const option = document.createElement("option");
+    option.value = type.id;
+    option.textContent = type.value;
+    activityTypeSelect.appendChild(option);
+  }
+}
+
+// Event listener when the page content has finished loading
+document.addEventListener("DOMContentLoaded", async () => {
+  const activityTypes = await fetchActivityTypes();
+  populateActivityTypes(activityTypes);
+});
   
   
   async function submitForm(event) {
@@ -125,6 +163,50 @@ function clearField(){
       }
   
     }
+    const formData = new FormData(event.target);
+    const data = {
+      first_name: formData.get("fullname").split(" ")[0],
+      last_name: formData.get("fullname").split(" ")[1],
+      student_id: parseInt(formData.get("studentID")),
+      email: formData.get("email"),
+      title: formData.get("workTitle"),
+      type_of_work_id: parseInt(formData.get("activityType")),
+      academic_year: parseInt(formData.get("academicYear")) - 543,
+      semester: parseInt(formData.get("semester")),
+      start_date: formData.get("startDate"),
+      end_date: formData.get("endDate"),
+      location: formData.get("location"),
+      description: formData.get("description")
+    };
+  
+    console.log(data);
+  
+    try {
+      const response = await fetch(`http://${window.location.hostname}:${port}/record`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+  
+      if (response.ok) {
+  
+        const formattedData = Object.entries(responseData.data)
+          .map(([key, value]) => `"${key}": "${value}"`)
+          .join("\n");
+  
+        document.getElementById("myForm").reset();
+      } else {
+        console.error("Failed to submit form data.");
+  
+
+        alert("Failed to submit form data. Please try again.");
+      }
+    } catch (error) {
+      console.error("An error occurred while submitting form data:", error);
+    }
+    
   
   
   
